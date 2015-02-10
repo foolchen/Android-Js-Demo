702ae91f35920dc920e45863ee7145d980084fa5
chenchong <chenchong8806@gmail.com>
2015-02-10 15:42:21 +0800

    本地JS完善

--- app/src/main/java/com/foolchen/js/MainActivity.java
+++ app/src/main/java/com/foolchen/js/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends ActionBarActivity {
         mWebSettings.setSupportZoom(true);
         mWebSettings.setUseWideViewPort(true);
         mWebSettings.setLoadWithOverviewMode(true);
+        mWebSettings.setTextZoom(100);// 默认文字缩放比为100%(该属性仅api14及以上可使用)
 
         mWebView.setWebViewClient(new WebViewClient() {
 
@@ -44,21 +45,47 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
-        mWebView.loadUrl("file:///android_asset/test.html");
+        //        mWebView.loadUrl("file:///android_asset/test2.html");
+        mWebView.loadUrl("http://a.xcar.com.cn/interface/6.0/bbs_detail.php?tid=21530314&p=1&type=0&deviceType=android&themeType=white&version=6.0.3B26&network=wifi");
     }
 
     @OnClick(R.id.button_decline)
     public void decline() {
-        mWebView.loadUrl("javascript:changeFontSize2('12px')");
+        //mWebView.loadUrl("javascript:changeFontSize('12px')");
+        changeFontSize(12);
     }
 
     @OnClick(R.id.button_reset)
     public void reset() {
-        mWebView.loadUrl("javascript:changeFontSize2('16px')");
+        //mWebView.loadUrl("javascript:changeFontSize('16px')");
+        changeFontSize(16);
     }
 
     @OnClick(R.id.button_rise)
     public void rise() {
-        mWebView.loadUrl("javascript:changeFontSize2('20px')");
+        //mWebView.loadUrl("javascript:changeFontSize('20px')");
+        changeFontSize(20);
     }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    private void changeFontSize(int size) {
+        String script = "javascript:(function changeFontSize(fontSize){\n" +
+                "            var d=document.getElementsByTagName(\"div\");\n" +
+                "            for(i=0;i<d.length;i++) {\n" +
+                "                var divNode = d[i];\n" +
+                "                if(divNode.className=='p_line')\n" +
+                "                    divNode.style.fontSize=\'" + size + "px\';\n" +
+                "            }\n" +
+                "        })()";
+        mWebView.loadUrl(script);
+    }
+
 }
